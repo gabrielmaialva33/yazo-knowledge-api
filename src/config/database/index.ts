f4af693c1a7a614410b7f38a3ab2ea182aa7c81b
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-06-24 11:14:25 -0300

    feat: set pre-struct api

---
+++ src/config/database/index.ts
@@ -0,0 +1,12 @@
+import * as AWS from 'aws-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
+export const dynamoDBClient = (): DocumentClient => {
+  if (process.env.IS_OFFLINE) {
+    return new AWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:5000',
+    })
+  }
+  return new AWS.DynamoDB.DocumentClient()
+}
---
+++ src/model/partner.ts
@@ -0,0 +1,9 @@
+export default interface Partner {
+  partner_id: string
+  category_id: string
+  title: object
+  sub_title: object
+  bio: object
+  created_at: string
+  updated_at: string
+}
