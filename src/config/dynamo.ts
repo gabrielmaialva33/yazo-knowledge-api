d1a6017fd28b49780fb377e6f7940397d909a48c
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-06-24 20:12:23 -0300

    refactor: dynamo config

--- src/config/dynamo.ts
+++ src/config/dynamo.ts
@@ -1,12 +1,5 @@
 import * as AWS from 'aws-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
-const dynamoDBClient = (): DocumentClient => {
-  if (process.env.IS_OFFLINE) {
-    return new AWS.DynamoDB.DocumentClient()
-  }
-
-  return new AWS.DynamoDB.DocumentClient()
-}
-
+const dynamoDBClient = (): DocumentClient => new AWS.DynamoDB.DocumentClient()
 export default dynamoDBClient
