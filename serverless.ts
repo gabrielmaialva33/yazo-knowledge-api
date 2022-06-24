96e4ff42a0d95c925a751628190a3e73d524e978
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-06-24 11:57:05 -0300

    feat: configure serverless plugins

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import hello from '@functions/hello'
 const serverlessConfiguration: AWS = {
   service: 'yazo-knowledge-api',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline', 'serverless-dynamodb-local'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -16,6 +16,25 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: [
+              'dynamodb:DescribeTable',
+              'dynamodb:Query',
+              'dynamodb:Scan',
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:DeleteItem',
+            ],
+            Resource: 'arn:aws:dynamodb:us-west-2:*:table/PartnerTable',
+          },
+        ],
+      },
     },
   },
   // import the function via paths
@@ -31,6 +50,40 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+    },
+    dynamodb: {
+      start: {
+        port: 5000,
+        inMemory: true,
+        migrate: true,
+      },
+      stages: 'dev',
+    },
+    resources: {
+      Resources: {
+        TodosTable: {
+          Type: 'AWS::DynamoDB::Table',
+          Properties: {
+            TableName: 'PartnerTable',
+            AttributeDefinitions: [
+              {
+                AttributeName: 'partner_id',
+                AttributeType: 'S',
+              },
+            ],
+            KeySchema: [
+              {
+                AttributeName: 'partner_id',
+                KeyType: 'HASH',
+              },
+            ],
+            ProvisionedThroughput: {
+              ReadCapacityUnits: 1,
+              WriteCapacityUnits: 1,
+            },
+          },
+        },
+      },
     },
   },
 }
